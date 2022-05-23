93ae4f37aa74de6cc95c0039bb24c2b90cb2cff4
1040168 <105462111+1040168@users.noreply.github.com>
2022-05-23 14:59:31 -0700

    Create Player.cs

---
+++ WumpusJones/WumpusJones/Player.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusJones
+{
+    class Player
+    {
+
+        public int Arrows { get; set; }
+        public double Inventory {get;set;}
+        public int Turns { get; set; }
+        public int Coins { get; set; }
+        public double Secret { get; set; }
+
+        public bool ShootArrows()
+        {
+            Turn();
+            if (Arrows > 0)
+            {
+                Arrows -= 1;
+                return true;
+            }
+                else return false;
+        }
+
+
+        public void ArrowPurchase()
+        {
+
+            Arrows++;
+            Turn();
+        }
+        public void SecretPurchase()
+        {
+
+            Turn();
+        }
+        private void Turn()
+        {
+            Turns++;
+        }
+
+        public void NoArrows()
+        {
+
+        }
+
+        //public 
+
+
+    }
+}
