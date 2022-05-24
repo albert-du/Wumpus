b60af07cc54a0d6df286d867ffc2b5f8fa179ffe
Matthew <105462114+matthewheike@users.noreply.github.com>
2022-05-23 21:48:46 -0700

    Game location (#15)
    * Update GameLocation.cs
    
    * Update GameLocation.cs

--- WumpusJones/WumpusJones/GameLocation.cs
+++ WumpusJones/WumpusJones/GameLocation.cs
@@ -8,17 +8,44 @@ namespace WumpusJones
 {
     class GameLocation
     {
-        public int PlayerRoom { get; set; }
-        public int WumpusRoom { get; set; }
-        public int BatRoom { get; set; }
-        public int HoleRoom { get; set; }
+        public Room PlayerRoom { get; set; }
+        public Room WumpusRoom { get; set; }
+        public Room BatRoom { get; set; }
+        public Room HoleRoom { get; set; }
 
-        public GameLocation(int p, int w, int b, int h)
+        Random rnd = new Random();
+        Cave cave;
+
+        public GameLocation(Cave cave)
         {
-            PlayerRoom = p;
-            WumpusRoom = w;
-            BatRoom = b;
-            HoleRoom = h;
+            this.cave = cave;
+        }
+        
+        public string MovePlayer(int room)
+        {
+            var neighbors = cave.Rooms[room - 1].Neighbors;
+            string value = "";
+            foreach (var r in neighbors)
+            {
+                if (r - 1 == WumpusRoom.Number)
+                {
+                    value += "Boulder nearby\n";
+                }
+                if (r - 1 == BatRoom.Number)
+                {
+                    value += "Bat nearby\n";
+                }
+                if (r - 1 == HoleRoom.Number)
+                {
+                    value += "Hole nearby\n";
+                }
+            }
+            return value;
+        }
+
+        private void CreateHazards()
+        {
+            
         }
     }
 }
