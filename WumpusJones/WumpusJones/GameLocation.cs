0439a96a18b1ba6994a077ef33e640999d3a507e
Matthew <105462114+matthewheike@users.noreply.github.com>
2022-05-24 17:33:40 -0700

    Game location (#16)
    * Update GameLocation.cs
    
    * Update GameLocation.cs
    
    * Update GameLocation.cs

--- WumpusJones/WumpusJones/GameLocation.cs
+++ WumpusJones/WumpusJones/GameLocation.cs
@@ -10,7 +10,8 @@ namespace WumpusJones
     {
         public Room PlayerRoom { get; set; }
         public Room WumpusRoom { get; set; }
-        public Room BatRoom { get; set; }
+        public Room BatRoom1 { get; set; }
+        public Room BatRoom2 { get; set; }
         public Room HoleRoom { get; set; }
 
         Random rnd = new Random();
@@ -20,7 +21,20 @@ namespace WumpusJones
         {
             this.cave = cave;
         }
-        
+
+        public void CreateHazards()
+        {
+            int bat1 = rnd.Next(30) + 1;
+            int bat2 = rnd.Next(30) + 1;
+            int wumpus = rnd.Next(30) + 1;
+            int hole = rnd.Next(30) + 1;
+
+            WumpusRoom = cave.Rooms[wumpus - 1];
+            BatRoom1 = cave.Rooms[bat1 - 1];
+            BatRoom2 = cave.Rooms[bat2 - 1];
+            HoleRoom = cave.Rooms[hole - 1];
+        }
+
         public string MovePlayer(int room)
         {
             var neighbors = cave.Rooms[room - 1].Neighbors;
@@ -29,23 +43,22 @@ namespace WumpusJones
             {
                 if (r - 1 == WumpusRoom.Number)
                 {
-                    value += "Boulder nearby\n";
+                    value += "You sense something huge nearby\n";
                 }
-                if (r - 1 == BatRoom.Number)
+                if (r - 1 == BatRoom1.Number || r - 1 == BatRoom2.Number)
                 {
-                    value += "Bat nearby\n";
+                    value += "Sounds of chittering bounce off the walls\n";
+                }
+                if (r - 1 == BatRoom1.Number && r - 1 == BatRoom2.Number)
+                {
+                    value += "Sounds are louder than usual\n";
                 }
                 if (r - 1 == HoleRoom.Number)
                 {
-                    value += "Hole nearby\n";
+                    value += "A draft blows through the room\n";
                 }
             }
             return value;
-        }
-
-        private void CreateHazards()
-        {
-            
         }
     }
 }
