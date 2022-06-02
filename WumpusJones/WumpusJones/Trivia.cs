422e0e00094beff73c9dab18205a2b63487980cc
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-06-01 17:12:15 -0700

    Trivia (#32)
    * add trivia forms
    
    * Delete Trivia.Designer.cs
    
    * Delete Trivia.resx
    
    Co-authored-by: 1081979 <105462247+1081979@users.noreply.github.com>

--- WumpusJones/WumpusJones/Trivia.cs
+++ WumpusJones/WumpusJones/Trivia.cs
@@ -10,8 +10,16 @@ namespace WumpusJones
 
         private readonly Question[] questions =
         {
-            new Question("Who is indiana jones?", "right", "wrong1", "wrong2" , "wrong3"),
-            new Question("Who is indiana jones?1", "right", "wrong1", "wrong2" , "wrong3"),
+            new Question("What is Indiana Jones afraid of?", "snakes", "dogs", "planes", "himself"),
+            new Question("Who plays Indiana Jones in the original three movies?", "Harrison Ford", "Dwayne Johnson", "Kevin Hart", "Tom Holand"),
+            new Question("What kind of dog does Indiana Jones have as a kid?", "Huskey", "Shiba Inu", "Golden retriever", "He didn't have a dog"),
+            new Question("In Temple of Doom, how many Shankara stones fall to the bottom the the ravine?", "2", "1", "3", "4"),
+            new Question("In which Indiana Jones movie does someone try to kill him?", "Raiders of the lost ark", "Indiana Jones and The Last Crusade", "Indiana Jones and The Temple of Doom", "All of them"),
+            new Question("What is Indiana Jones' trademark weapon?", "A bullwhip", "a sword", "a machine gun", "a laser"),
+            new Question("When did the first Indiana Jones movie come out?", "1989", "1999", "2005", "1985"),
+            new Question("What color is Indiana Jones' hat?", "brown", "red", "black", "white"),
+            new Question("Where does Indiana Jones teach archaeology?", "Marshall college", "Norwalk College", "Trinity College", "Goodwin College"),
+            new Question("What is the name of the cult in Indiana Jones and the Temple of Doom?", "The people's Temple", "Thuggers", "Raelians", "Heaven's Gate")
         };
 
         public Trivia() =>
