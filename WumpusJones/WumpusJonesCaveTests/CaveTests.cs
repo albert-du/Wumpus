34fe0c4e6e4dda4ccd3089680d84b2a300b1b631
1047506 <105462101+1047506@users.noreply.github.com>
2022-06-03 20:25:08 -0700

    cave Unit tests finsiihed (#43)
    * Unit Test
    
    * Update PlayerTest.cs
    
    * Update dotnet.yml
    
    * Update dotnet.yml
    
    * Update WumpusJones.Tests.csproj
    
    * fix build
    
    * CaveTests
    
    * cave unit tests finished
    
    * Delete WumpusJones.Tests.csproj
    
    Co-authored-by: 1040168 <105462111+1040168@users.noreply.github.com>
    Co-authored-by: Albert Du <52804499+albert-du@users.noreply.github.com>

---
+++ WumpusJones/WumpusJonesCaveTests/CaveTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using WumpusJones;
+
+namespace WumpusJonesCaveTests
+{
+    [TestClass]
+    public class CaveTests
+    {
+        Cave Cave = new Cave(1);
+
+        [TestMethod]
+        public void TestExploredRoom()
+        {
+            //testing to see if explored goes up by 1 once
+            //go into a room that hasn't been explored
+            Cave.ExploredRoom(1);
+            var explored = Cave.Explored;
+
+            Assert.IsTrue(explored[0] == 1);
+        }
+
+        [TestMethod]
+        public void TestNotExploredRoom()
+        {
+            //testing to see if not explored, number does not increase
+            Cave.ExploredRoom(1);
+            var explored = Cave.Explored;
+
+            Assert.IsFalse(explored[0] == 2);
+        }
+
+        [TestMethod]
+        public void CheckRoomAt1()
+        {
+            //testing to see if it sees what room you are in
+            //also to test adjacent rooms
+            Room room = Cave.RoomAt(1);
+
+            Assert.AreEqual(room.Neighbors[0], -25);
+            Assert.IsTrue(room.Number == 1);
+        }
+
+        [TestMethod]
+        public void CheckRoomAt3()
+        {
+            //testing to see if it sees what room you are in
+            //also to test adjacent rooms
+            Room room = Cave.RoomAt(3);
+
+            Assert.AreEqual(room.Neighbors[0], -27);
+            Assert.IsTrue(room.Number == 3);
+        }
+
+        [TestMethod]
+        public void NotInRoomAt1()
+        {
+            //testing to see if it sees you are in a different room
+            Room room = Cave.RoomAt(1);
+
+            Assert.IsFalse(room.Number == 3);
+        }
+
+        [TestMethod]
+        public void NotInRoomAt3()
+        {
+            //testing to see if it sees you are in a different room
+            Room room = Cave.RoomAt(3);
+
+            Assert.IsFalse(room.Number == 1);
+        }
+    }
+}
---
+++ WumpusJones/WumpusJonesCaveTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WumpusJonesCaveTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WumpusJonesCaveTests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("43517417-056a-4ad9-aa52-becbe3d8ddc7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
